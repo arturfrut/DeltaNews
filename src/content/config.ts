098993d4eb6a8f8bc59b1755a2a82f7ddaa19cc7
arturfrut <arturfruttero@gmail.com>
2024-03-27 13:18:12 -0300

    fix error in categories

--- src/content/config.ts
+++ src/content/config.ts
@@ -13,6 +13,7 @@ const blog = defineCollection({
 				.or(z.date())
 				.transform((val) => new Date(val)),
 			heroImage: image(),
+			category: z.enum(CATEGORIES).default('Category 1'),
 			tags: z.array(z.string()),
 			draft: z.boolean().default(false)
 		})
